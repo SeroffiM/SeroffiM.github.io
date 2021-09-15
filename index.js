87085c138f43dcb168fd930bd1c32604631ec551
SeroffiM <roma.serzhanovich80@gmail.com>
2021-09-15 14:03:48 +0300

    feat: add header

---
+++ index.js
@@ -0,0 +1,25 @@
+const headerBurger = document.querySelector('.header__burger'),
+      headerNav = document.querySelector('.header__nav')
+
+headerBurger.addEventListener('click',()=>{
+  headerBurger.classList.toggle('active')
+  headerNav.classList.toggle('active')
+  document.body.classList.toggle('overflow')
+})
+
+const headerMenu = document.querySelector('.header__menu'),
+      headerLogoSpan = document.querySelector('.header__logo-span')
+
+stickyHeader = () => {
+  const position = window.scrollY
+  if(position>20){
+    headerMenu.classList.add('sticky')
+    headerLogoSpan.classList.add('active')
+  } else if (position < 20) {
+    headerMenu.classList.remove('sticky')
+    headerLogoSpan.classList.remove('active')
+  }
+  console.log(position);
+}
+
+window.addEventListener('scroll',stickyHeader)
