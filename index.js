3e5c9b500189a9fdebca68c4eda0a4e4ea1c01b7
SeroffiM <roma.serzhanovich80@gmail.com>
2021-09-17 22:20:28 +0300

    fix: burger menu

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const headerBurger = document.querySelector('.header__burger'),
       headerNav = document.querySelector('.header__nav')
 
-headerBurger.addEventListener('click',()=>{
+const linkBurger = document.querySelectorAll('.header__nav-link')
+
+
+changeMenu = () => {
   headerBurger.classList.toggle('active')
   headerNav.classList.toggle('active')
   document.body.classList.toggle('overflow')
-})
+}
 
+headerBurger.addEventListener('click',changeMenu)
+
+linkBurger.forEach(item=>{
+  item.addEventListener('click',changeMenu)
+})
 const headerMenu = document.querySelector('.header__menu'),
       headerLogoSpan = document.querySelector('.header__logo-span')
 
